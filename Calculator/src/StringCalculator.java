a581906d65276d5a0b3604c6f8a8d05930a8daef
Vivek <vivekswami84@gmail.com>
2020-11-09 11:54:35 +0530

    added test testAdd_withOneNumber and equivalent code to fix the test.

--- Calculator/src/StringCalculator.java
+++ Calculator/src/StringCalculator.java
@@ -1,5 +1,8 @@
 public class StringCalculator {
     public int add(String numbers) {
+        if(numbers.length() > 0){
+            return Integer.parseInt(numbers);
+        }
         return 0;
     }
 }
--- Calculator/test/TestStringCalculator.java
+++ Calculator/test/TestStringCalculator.java
@@ -8,4 +8,11 @@ public class TestStringCalculator {
     public void testAdd_withEmptyString(){
         Assert.assertEquals(0, stringCalculator.add(""));
     }
+
+    @Test
+    public void testAdd_withOneNumber(){
+        int number = 12;
+        Assert.assertEquals(number, stringCalculator.add("12"));
+    }
+
 }
